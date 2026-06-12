3e0e0e3d8ad57f461b75f4a18ab3e50e90f115c1
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-12 20:37:42 +0700

    Perbaikan Tahap 3 (Tiket Reguler)

--- class/TiketReguler.php
+++ class/TiketReguler.php
@@ -1,27 +1,24 @@
 <?php
-require_once 'Tiket.php';
+require_once __DIR__ . '/../Tiket.php';
 
 class TiketRegular extends Tiket {
-    // Properti spesifik kelas Regular
     protected $tipeAudio;
     protected $lokasiBaris;
 
     public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $harga_dasar_tiket, $tipeAudio, $lokasiBaris) {
-        // Mengirim data ke constructor abstract class Tiket
         parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $harga_dasar_tiket);
         $this->tipeAudio = $tipeAudio;
         $this->lokasiBaris = $lokasiBaris;
     }
 
-    // Getter & Setter Spesifik
     public function getTipeAudio() { return $this->tipeAudio; }
-    public function setTipeAudio($tipeAudio) { $this->tipeAudio = $tipeAudio; }
-
     public function getLokasiBaris() { return $this->lokasiBaris; }
-    public function setLokasiBaris($lokasiBaris) { $this->lokasiBaris = $lokasiBaris; }
 
-    // Implementasi dari method abstract induk
-    public function getJenisStudio() {
-        return 'Regular';
+    public function getJenisStudio() { return 'Regular'; }
+
+    // Tahap 5: Overriding Hitung Harga
+    public function hitungTotalHarga() {
+        return $this->jumlah_kursi * $this->harga_dasar_tiket;
     }
 }
+?>
